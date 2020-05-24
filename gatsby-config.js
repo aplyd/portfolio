4497d37e20bbae48811b3734e8c6690883e348fb
aplyd <austin@ftacnik.com>
2020-05-23 18:32:14 -0700

    hmmmmmm

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,11 @@ module.exports = {
 				icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
 			}
 		},
-		`gatsby-plugin-styled-components`
+		{
+			resolve: 'gatsby-plugin-styled-components',
+			options: { displayName: false }
+		}
+
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
