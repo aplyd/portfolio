43929fba1a9e571d69c8d5e9e14a101b24d0ed3f
aplyd <austin@ftacnik.com>
2020-09-09 16:23:32 -0700

    remove gatsby-awesome-pagination and revert previous changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,9 @@
 const path = require('path');
-const { paginate } = require('gatsby-awesome-pagination');
 
-exports.createPages = async ({ actions, graphql }) => {
+exports.createPages = ({ actions, graphql }) => {
 	const { createPage } = actions;
-
-	// const blogPost = path.resolve('src/templates/blog-post.js');
-	const blogPosts = await graphql(`
+	const blogPost = path.resolve('src/templates/blog-post.js');
+	return graphql(`
 		{
 			allMarkdownRemark {
 				edges {
@@ -22,43 +20,16 @@ exports.createPages = async ({ actions, graphql }) => {
 				}
 			}
 		}
-	`);
+	`).then(res => {
+		if (res.errors) {
+			return Promise.reject(res.errors);
+		}
 
-	paginate({
-		createPage,
-		items: blogPosts,
-		itemsPerPage: 2,
-		pathPrefix: '/blog',
-		component: path.resolve('src/template/blog.js')
+		res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+			createPage({
+				path: node.frontmatter.path,
+				component: blogPost
+			});
+		});
 	});
-
-	// return graphql(`
-	// {
-	//     allMarkdownRemark {
-	//         edges {
-	//             node {
-	//                 html
-	//                 id
-	//                 frontmatter {
-	//                     path
-	//                     title
-	//                     date
-	//                     author
-	//                 }
-	//             }
-	//         }
-	//     }
-	// }
-	// `).then(res => {
-	//     if (res.errors) {
-	//         return Promise.reject(res.errors)
-	//     }
-
-	//     res.data.allMarkdownRemark.edges.forEach(({node}) => {
-	//         createPage({
-	//             path: node.frontmatter.path,
-	//             component: postTemplate
-	//         })
-	//     })
-	// })
 };
