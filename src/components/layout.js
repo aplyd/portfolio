b692be1b05c63cb4399f08058d3729fd94ed3a9c
aplyd <austin@ftacnik.com>
2020-09-21 16:55:03 -0700

    add this sweet background gradient

--- src/components/layout.js
+++ src/components/layout.js
@@ -17,6 +17,15 @@ import styled from 'styled-components';
 import { VscClose } from 'react-icons/vsc';
 import { motion, AnimatePresence } from 'framer-motion';
 
+const Container = styled.div`
+	background-color: #4c606b;
+	background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1200 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='0' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23395b5c'/%3E%3Cstop offset='1' stop-color='%23395b5c' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='1200' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%237b9e8d'/%3E%3Cstop offset='1' stop-color='%237b9e8d' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='c' cx='600' cy='0' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23879270'/%3E%3Cstop offset='1' stop-color='%23879270' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='600' cy='800' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%234c606b'/%3E%3Cstop offset='1' stop-color='%234c606b' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='e' cx='0' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23365445'/%3E%3Cstop offset='1' stop-color='%23365445' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='f' cx='1200' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ebd1a9'/%3E%3Cstop offset='1' stop-color='%23ebd1a9' stop-opacity='0'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='1200' height='800'/%3E%3Crect fill='url(%23b)' width='1200' height='800'/%3E%3Crect fill='url(%23c)' width='1200' height='800'/%3E%3Crect fill='url(%23d)' width='1200' height='800'/%3E%3Crect fill='url(%23e)' width='1200' height='800'/%3E%3Crect fill='url(%23f)' width='1200' height='800'/%3E%3C/svg%3E");
+	background-attachment: fixed;
+	background-size: cover;
+	width: 100%;
+	height: 100%;
+`;
+
 const MobileMenu = styled(motion.div)`
 	background-color: var(--color-dark);
 	position: fixed;
@@ -93,7 +102,7 @@ const Layout = ({ children }) => {
 	};
 
 	return (
-		<>
+		<Container>
 			<GlobalStyle />
 			{/* TODO - animate with framer-motion */}
 			<AnimatePresence>
@@ -198,7 +207,7 @@ const Layout = ({ children }) => {
 			<PageContainer>
 				<main>{children}</main>
 			</PageContainer>
-		</>
+		</Container>
 	);
 };
 
